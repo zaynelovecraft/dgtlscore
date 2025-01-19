e66f7f79e233c88b5fd95cd9b16bcf27a31c1849
dgtlpros <capitalizedcashflow@gmail.com>
2025-01-19 07:02:29 -0700

    feat: update logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -33,18 +33,18 @@ function Hero() {
               href="/blog/radiant-raises-100m-series-a-from-tailwind-ventures"
               className="flex items-center gap-1 rounded-full bg-fuchsia-950/35 px-3 py-0.5 text-sm/6 font-medium text-white data-[hover]:bg-fuchsia-950/30"
             >
-              Radiant raises $100M Series A from Tailwind Ventures
+              DGTL SCORE raises $100M Series A from blockchain Ventures
               <ChevronRightIcon className="size-4" />
             </Link>
           }
         />
         <div className="pb-24 pt-16 sm:pb-32 sm:pt-24 md:pb-48 md:pt-32">
           <h1 className="font-display text-balance text-6xl/[0.9] font-medium tracking-tight text-gray-950 sm:text-8xl/[0.8] md:text-9xl/[0.8]">
-            Close every deal.
+            Blockchain credit scores, loans, and insurance.
           </h1>
           <p className="mt-8 max-w-lg text-xl/7 font-medium text-gray-950/75 sm:text-2xl/8">
-            Radiant helps you sell more by revealing sensitive information about
-            your customers.
+            We’re bridging AI and blockchain technologies to provide secure,
+            intelligent financial solutions.
           </p>
           <div className="mt-12 flex flex-col gap-x-6 gap-y-4 sm:flex-row">
             <Button href="#">Get started</Button>
--- src/components/gradient.tsx
+++ src/components/gradient.tsx
@@ -9,7 +9,7 @@ export function Gradient({
       {...props}
       className={clsx(
         className,
-        'bg-[linear-gradient(115deg,var(--tw-gradient-stops))] from-[#fff1be] from-[28%] via-[#ee87cb] via-[70%] to-[#b060ff] sm:bg-[linear-gradient(145deg,var(--tw-gradient-stops))]',
+        'bg-[linear-gradient(115deg,var(--tw-gradient-stops))] from-[#e3ffe3] from-[28%] via-[#1c93a3] via-[70%] to-[#1c7aa3ff] sm:bg-[linear-gradient(145deg,var(--tw-gradient-stops))]',
       )}
     />
   )
@@ -21,7 +21,7 @@ export function GradientBackground() {
       <div
         className={clsx(
           'absolute -right-60 -top-44 h-60 w-[36rem] transform-gpu md:right-0',
-          'bg-[linear-gradient(115deg,var(--tw-gradient-stops))] from-[#fff1be] from-[28%] via-[#ee87cb] via-[70%] to-[#b060ff]',
+          'bg-[linear-gradient(115deg,var(--tw-gradient-stops))] from-[#e3ffe3] from-[28%] via-[#1c93a3] via-[70%] to-[#1c7aa3ff]',
           'rotate-[-10deg] rounded-full blur-3xl',
         )}
       />
